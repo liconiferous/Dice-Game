d624c37fe80e5b4d5a5a941102cef1e99cd424a8
liconiferous <liconiferous@163.com>
2024-05-23 01:22:36 +0200

    Update game.py
    Changed wrong function call to new function call

--- src/game.py
+++ src/game.py
@@ -45,7 +45,7 @@ class Game:
 
     def play_game(self):
             self.play_turn()
-            if not isinstance(self.current_player, NPC) and self.dice.GetTotalValue() > 0:
+            if not isinstance(self.current_player, NPC) and self.dice.get_turn_value() > 0:
                 print('if you want to hold, write the command hold. Otherwise just hit enter to continue the turn')
 
     def reset_game(self):
